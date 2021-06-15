dc66e7d197edf1b862874ecf690d87c0bf6ee668
Rodney Mupanduki <rodneymupanduki@gmail.com>
2021-06-15 20:37:39 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function turnZebra() {
+    let rows = document.querySelectorAll('tr:nth-child(odd)');
+    rows.forEach((row) => {
+        row.classList.add('zebraStripe');
+    });   
+}
+
+function removeZebra() {
+    let rows = document.querySelectorAll('tr:nth-child(odd)');
+    rows.forEach((row) => {
+        row.classList.remove('zebraStripe');
+    });   
+}
